6e3d0310d9081a3911ca7cce0771126d731157ca
CassidyHeulings <116444438+CassidyHeulings@users.noreply.github.com>
2023-12-04 23:01:23 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Careers from "./Careers"
+import ContactUs from "./ContactUs"
+import Main from "./Main"
+import OnlineOrder from "./OnlineOrdering/OnlineOrder"
+import Directions from "./Directions"
+
+export default function App() {
+    return(
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element = {<Main />} />
+                    <Route path = "/Careers" element = {<Careers />} />
+                    <Route path = "/ContactUs" element = {<ContactUs />} />
+                    <Route path = "/Main" element = {<Main />} />
+                    <Route path = "/OnlineOrder" element = {<OnlineOrder />} />
+                    <Route path = "/Directions" element = {<Directions />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
