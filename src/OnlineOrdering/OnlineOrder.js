33d5ea2df48d59232516c7202969056dd3bb6e4d
CassidyHeulings <116444438+CassidyHeulings@users.noreply.github.com>
2023-12-04 23:02:16 -0500

    Add files via upload

---
+++ src/OnlineOrdering/OnlineOrder.js
@@ -0,0 +1,52 @@
+//import './OnlineOrder.css';
+import React from 'react';
+import leaves from './leaves.png';
+import { Link } from "react-router-dom";
+
+console.log(leaves);
+document.body.style = 'background: #FBBA72;';
+
+function OnlineOrder() {
+ return (
+  <div>
+    <h1>Online Ordering</h1>
+    <nav class="navbar background">
+      <ul class="nav-list">
+        <li>
+          <Link to="/Main">Home</Link>
+        </li>
+        <li>
+          <Link to="https://www.google.com">Menu</Link>
+        </li>
+        <li>
+          <Link to="/Careers">Career</Link>
+        </li>
+        <li>
+          <Link to="/Directions">Directions</Link>
+        </li>
+        <li>
+          <Link to="/ContactUs">Contact Us</Link>
+        </li>
+      </ul>
+      <div class="rightNav">
+        <button>Pickup</button>
+        <button>Delivery</button>
+      </div>
+    </nav>
+
+    <div class="menu">
+      <p>Menu</p>
+    </div>
+
+    <div class="summary">
+      <p>Order Summary</p>
+    </div>
+    
+    <div class="img">
+      <img src={leaves} alt="Fall Leaves" />
+    </div>
+  </div>
+ )
+}
+
+export default OnlineOrder;
